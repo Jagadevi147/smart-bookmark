0ce570c6a49ea95c506473181fc55f558ff1ac6a
Jagadevi147 <jagadevij147@gmail.com>
2026-02-16 18:09:07 +0530

    Smart Bookmark App completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,168 @@
-import Image from "next/image";
+"use client";
+
+import { useEffect, useState } from "react";
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+);
 
 export default function Home() {
+  const [user, setUser] = useState<any>(null);
+  const [title, setTitle] = useState("");
+  const [url, setUrl] = useState("");
+  const [bookmarks, setBookmarks] = useState<any[]>([]);
+
+  // get logged user
+  useEffect(() => {
+    const getUser = async () => {
+      const { data } = await supabase.auth.getUser();
+      if (data.user) {
+        setUser(data.user);
+        fetchBookmarks(data.user.id);
+      }
+    };
+    getUser();
+  }, []);
+
+  // realtime updates
+  useEffect(() => {
+    if (!user) return;
+
+    const channel = supabase
+      .channel("realtime-bookmarks")
+      .on(
+        "postgres_changes",
+        {
+          event: "*",
+          schema: "public",
+          table: "bookmarks",
+          filter: `user_id=eq.${user.id}`,
+        },
+        () => fetchBookmarks(user.id),
+      )
+      .subscribe();
+
+    return () => {
+      supabase.removeChannel(channel);
+    };
+  }, [user]);
+
+  const fetchBookmarks = async (userId: string) => {
+    const { data } = await supabase
+      .from("bookmarks")
+      .select("*")
+      .eq("user_id", userId)
+      .order("created_at", { ascending: false });
+
+    setBookmarks(data || []);
+  };
+
+  const addBookmark = async () => {
+    if (!title || !url) return alert("Enter title & URL");
+
+    await supabase.from("bookmarks").insert([{ title, url, user_id: user.id }]);
+
+    setTitle("");
+    setUrl("");
+  };
+
+  const deleteBookmark = async (id: string) => {
+    await supabase.from("bookmarks").delete().eq("id", id);
+  };
+
+  const loginWithGoogle = async () => {
+    await supabase.auth.signInWithOAuth({ provider: "google" });
+  };
+
+  const logout = async () => {
+    await supabase.auth.signOut();
+    setUser(null);
+  };
+
+  // LOGIN SCREEN
+  if (!user) {
+    return (
+      <div className="flex items-center justify-center min-h-screen bg-gray-100">
+        <div className="bg-white shadow-lg rounded-xl p-8 text-center">
+          <h1 className="text-3xl font-bold mb-6">Smart Bookmark</h1>
+          <button
+            onClick={loginWithGoogle}
+            className="bg-blue-600 text-white px-6 py-2 rounded-lg hover:bg-blue-700"
+          >
+            Sign in with Google
+          </button>
+        </div>
+      </div>
+    );
+  }
+
+  // MAIN APP
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
+    <div className="min-h-screen bg-gray-100 flex justify-center pt-12">
+      <div className="bg-white shadow-xl rounded-xl p-8 w-full max-w-md">
+        <h2 className="text-xl font-semibold text-center mb-2">Welcome</h2>
+        <p className="text-center text-gray-500 mb-4">{user.email}</p>
+
+        <button
+          onClick={logout}
+          className="w-full bg-red-500 text-white py-2 rounded-lg mb-6 hover:bg-red-600"
+        >
+          Logout
+        </button>
+
+        <h3 className="font-semibold mb-2">Add Bookmark</h3>
+
+        <input
+          className="w-full border p-2 rounded mb-2"
+          placeholder="Title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
         />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+
+        <input
+          className="w-full border p-2 rounded mb-3"
+          placeholder="URL"
+          value={url}
+          onChange={(e) => setUrl(e.target.value)}
+        />
+
+        <button
+          onClick={addBookmark}
+          className="w-full bg-green-600 text-white py-2 rounded-lg hover:bg-green-700"
+        >
+          Add Bookmark
+        </button>
+
+        <h3 className="font-semibold mt-6 mb-2">Your Bookarks</h3>
+
+        {bookmarks.length === 0 && (
+          <p className="text-gray-400 text-sm">No bookmarks yet</p>
+        )}
+
+        {bookmarks.map((b) => (
+          <div
+            key={b.id}
+            className="flex justify-between items-center bg-gray-50 p-2 rounded mb-2"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+            <a
+              href={b.url}
+              target="_blank"
+              className="text-blue-600 hover:underline"
+            >
+              {b.title}
+            </a>
+
+            <button
+              onClick={() => deleteBookmark(b.id)}
+              className="text-red-500"
+            >
+              ❌
+            </button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
